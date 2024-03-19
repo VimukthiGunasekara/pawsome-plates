8c45eb3376a5fee1039601781097bbf4f092ec5f
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2024-03-19 17:59:04 -0400

    add hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import CustomImage from "./CustomImage";
+
+export default function HeroSection() {
+    const images = [
+        "/image/1.png",
+        "/image/2.png",
+        "/image/3.png",
+        "/image/4.png"
+    ]
+    return (
+        <div className="section hero">
+            <div className="col typography">
+                <h1 className="title">Your Dog's Perfect Homemade Recipe!</h1>
+                <p className="info">Embark on a culinary adventure with our paw-some recipe, meticulously crafted with love and care 🐾 Prepare for a tail-wagging experience filled with delicious goodness, guaranteed to make your furry friend's day an unforgettable delight! 🐶.</p>
+                <button className="btn">Explore Now</button>
+            </div>
+            <div className="col gallery">
+                {images.map((src, index) => (
+                    <CustomImage key={index} imgsrc={src} />
+                ))}
+            </div>
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,45 @@
+import { useState } from "react"
+import Sidebar from "./Sidebar"
+
 export default function Navbar() {
+
+    const [showSidebar, setShowSidebar] = useState(false)
+    const links = [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "Recipes",
+            path: "/recipes"
+        },
+        {
+            name: "Settings",
+            path: "/settings"
+        }
+    ]
+
+    function closeSidebar() {
+        setShowSidebar(false)
+    }
+
     return (
-        <div className="navbar container">
-            <a className="logo" href="#!">Pawsome<span> Plates</span></a>
-            <div className="nav-links">
-                <a className="active" href="#!">Home</a>
-                <a href="#!">Recipes</a>
-                <a href="#!">Settings</a>
+        <>
+            <div className="navbar container">
+                <a className="logo" href="#!">Pawsome<span> Plates</span></a>
+                <div className="nav-links">
+                    {links.map(link => (
+                        <a href="#!" key={link.name}>{link.name}</a>
+                    ))}
+                </div>
+                <div onClick={() => setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
             </div>
-        </div>
+            {showSidebar && <Sidebar close={closeSidebar} links={links} />}
+
+        </>
     )
 }
