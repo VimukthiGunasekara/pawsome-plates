8ab6d65357ac7acf803cd61a5918f2170b68a0dc
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2024-07-05 13:15:53 -0400

    update images path6

--- src/components/Community.js
+++ src/components/Community.js
@@ -6,22 +6,22 @@ export default function CommunitySection() {
             <div class="community-section">
 
                 <div class="community-item item1">
-                    <img src="pawsome-plates/image/community/1.png" alt="community 3" />
+                    <img src="/pawsome-plates/image/community/1.png" alt="community 3" />
                 </div>
                 <div class="community-item item2">
-                    <img src="pawsome-plates/image/community/2.png" alt="community 1" />
+                    <img src="/pawsome-plates/image/community/2.png" alt="community 1" />
                 </div>
                 <div class="community-item item3">
-                    <img src="pawsome-plates/image/community/3.png" alt="community 2" />
+                    <img src="/pawsome-plates/image/community/3.png" alt="community 2" />
                 </div>
                 <div class="community-item item4">
-                    <img src="pawsome-plates/image/community/4.png" alt="community 3" />
+                    <img src="/pawsome-plates/image/community/4.png" alt="community 3" />
                 </div>
                 <div class="community-item item5">
-                    <img src="pawsome-plates/image/community/5.png" alt="community 4" />
+                    <img src="/pawsome-plates/image/community/5.png" alt="community 4" />
                 </div>
                 <div class="community-item item6">
-                    <img src="pawsome-plates/image/community/6.png" alt="community 1" />
+                    <img src="/pawsome-plates/image/community/6.png" alt="community 1" />
                 </div>
             </div>
         </div>
